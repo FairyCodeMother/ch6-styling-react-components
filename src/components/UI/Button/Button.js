853dc4789821f14727ce43c566adc381f26d7502
ginahall <gina.hall@storable.com>
2021-12-10 18:51:12 -0600

    Ch 6- Styling by Component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-// 7. We're using Modules in our dynamic styling which requires us to use the
-//    following syntax update, then update the CSS's file name to: Button.module.css.
-//    This turns each className into a callable property scoped to this Component.
-import styles from './Button.module.css';
+import './Button.css';
 
-// 8. Update the className to call the button property.
-// Do the Same to CourseInput.
 const Button = props => {
   return (
-    <button type={props.type} className={ styles.button } onClick={props.onClick}>
+    <button type={props.type} className="button" onClick={props.onClick}>
       {props.children}
     </button>
   );
